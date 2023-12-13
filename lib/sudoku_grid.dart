84e19f2257d7934c21f4b494efef84d42d69034f
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-13 10:14:17 +0100

    Updates

---
+++ lib/sudoku_grid.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SudokuGrid extends StatefulWidget {
+  const SudokuGrid({super.key});
+
+  @override
+  State<SudokuGrid> createState() => _SudokuGridState();
+}
+
+class _SudokuGridState extends State<SudokuGrid> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: GridV);
+  }
+}
