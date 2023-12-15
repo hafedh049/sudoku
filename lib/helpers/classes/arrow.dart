75748349ed1c6d8299ab608b8df3f72b4c0efb39
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-15 10:19:46 +0100

    ModifiedM lib/helpers/classes/arrow.dart

--- lib/helpers/classes/arrow.dart
+++ lib/helpers/classes/arrow.dart
@@ -12,10 +12,10 @@ class _ArrowState extends State<Arrow> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onLongPressDown: (details) {},
+      onLongPressDown: (LongPressDownDetails details) {},
       onLongPressUp: () {},
-      onTapUp: (details) {},
-      onTapDown: (details) {},
+      onTapUp: (TapUpDetails details) {},
+      onTapDown: (TapDownDetails details) {},
       child: Container(
         padding: const EdgeInsets.all(16),
         decoration: BoxDecoration(border: Border.all(width: .5, color: null), shape: BoxShape.circle),
