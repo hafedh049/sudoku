ea53c0f3bc42bb5c0b7b80de68f27e91fc623708
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-15 09:54:28 +0100

    Modified  lib/helpers/classes/arrow.dart

--- lib/helpers/classes/arrow.dart
+++ lib/helpers/classes/arrow.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
+import 'package:pretty_animated_buttons/widgets/pretty_wave_button.dart';
 
 class Arrow extends StatefulWidget {
-  const Arrow({super.key});
-
+  const Arrow({super.key,required this.arrow});
+  final IconData arrow;
   @override
   State<Arrow> createState() => _ArrowState();
 }
@@ -10,6 +12,6 @@ class Arrow extends StatefulWidget {
 class _ArrowState extends State<Arrow> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const PrettyWaveButton(onPressed: () {  }, child: Icon(FontAwesome.),);
   }
 }
