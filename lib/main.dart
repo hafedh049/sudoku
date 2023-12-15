ebc2959cd83e578a6e22f6c625bbc94166e70f5f
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-15 09:30:53 +0100

    Modified  lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,6 @@ class Main extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp();
+    return GetMaterialApp(debugShowCheckedModeBanner: false,home: ,);
   }
 }
