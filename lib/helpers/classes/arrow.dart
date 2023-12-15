e6899f2d0ae4bef7cce7236037ce25ac95cb68d7
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-15 10:32:20 +0100

    Modified  lib/helpers/classes/arrow.dart

--- lib/helpers/classes/arrow.dart
+++ lib/helpers/classes/arrow.dart
@@ -25,19 +25,16 @@ class _ArrowState extends State<Arrow> {
       onLongPressUp: () async {
         setState(() => _borderState = false);
         await Future.delayed(100.ms);
-
         setState(() => _colorState = true);
       },
       onTapUp: (TapUpDetails details) async {
         setState(() => _borderState = false);
         await Future.delayed(100.ms);
-
         setState(() => _colorState = true);
       },
       onTapDown: (TapDownDetails details) async {
         setState(() => _borderState = true);
         await Future.delayed(100.ms);
-
         setState(() => _colorState = true);
       },
       child: AnimatedContainer(
