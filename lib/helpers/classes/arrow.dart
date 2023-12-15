b30eac08b2d4f54fb0661b4163d6ba5accb60541
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-15 09:46:27 +0100

    Modified  lib/helpers/classes/arrow.dart

---
+++ lib/helpers/classes/arrow.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Arrow extends StatefulWidget {
+  const Arrow({super.key});
+
+  @override
+  State<Arrow> createState() => _ArrowState();
+}
+
+class _ArrowState extends State<Arrow> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
