4e7397378df4453def11c584280aca80608975cf
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-15 10:46:53 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -73,7 +73,7 @@ class _HomeState extends State<Home> {
                           ),
                         ),
                         StatefulBuilder(
-                          key: _leftArrowKey,
+                          key: _rightArrowKey,
                           builder: (context, snapshot) {
                             return Arrow(
                               arrow: FontAwesome.chevron_left,
