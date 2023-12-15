cdf52e468ad11a06cd10abca9c153a3b84223da5
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-15 10:10:06 +0100

    Modified  lib/helpers/utils.dart

--- lib/helpers/utils.dart
+++ lib/helpers/utils.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const Color gold = Color.fromARGB(255, 255, 215, 0);
+const Color gold = Color.fromARGB(255, 189, 181, 138);
 const Color dark = Color.fromARGB(255, 0, 0, 0);
 const Color white = Color.fromARGB(255, 255, 255, 255);
 const Color transparent = Colors.transparent;
