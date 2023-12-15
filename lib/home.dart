d8889df8f0e6ba133e972f9b846970e87a4d9d63
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-15 10:57:01 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -62,7 +62,7 @@ class _HomeState extends State<Home> {
                           builder: (BuildContext context, void Function(void Function()) _) {
                             return AnimatedOpacity(
                               duration: 300.ms,
-                              opacity: _hideLeftArrow ? 1 : 0,
+                              opacity: _hideLeftArrow ? 0 : 1,
                               child: Arrow(
                                 arrow: FontAwesome.chevron_left,
                                 callback: () {
