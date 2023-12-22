9a4abcb0d26effaa024bc937b42863c2486fc756
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-22 15:18:32 +0100

    Modified  lib/sudoku_grid.dart

--- lib/sudoku_grid.dart
+++ lib/sudoku_grid.dart
@@ -51,9 +51,10 @@ class _SudokuGridState extends State<SudokuGrid> {
                                           ),
                                         ),
                                       ),
-                                      if() ...<Widget>[  const SizedBox(width: 5),
-                                      Container(width: 1, height: 20, color: gray),],
-
+                                      if (columnIndex % 2 != 0) ...<Widget>[
+                                        const SizedBox(width: 5),
+                                        Container(width: 1, height: 20, color: gray),
+                                      ],
                                     ],
                                   ),
                                   Container(width: 20, height: 1, color: gray),
