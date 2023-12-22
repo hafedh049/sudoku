220884b55c6145a7ad51125da32129018b78c214
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-22 15:29:36 +0100

    ModifiedM lib/sudoku_grid.dart

--- lib/sudoku_grid.dart
+++ lib/sudoku_grid.dart
@@ -26,50 +26,53 @@ class _SudokuGridState extends State<SudokuGrid> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: <Widget>[
-            for (int rowIndex = 1; rowIndex <= 9; rowIndex += 1)
-              Flexible(
-                child: Row(
-                  children: <Widget>[
-                    for (int columnIndex = 1; columnIndex <= 9; columnIndex += 1)
-                      _grid[rowIndex - 1][columnIndex - 1] != 0
-                          ? Expanded(
-                              child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                children: <Widget>[
-                                  Row(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: <Widget>[
-                                      Container(
-                                        padding: const EdgeInsets.all(8),
-                                        decoration: const BoxDecoration(shape: BoxShape.circle, color: gray),
-                                        child: Center(
-                                          child: Text(
-                                            _grid[rowIndex - 1][columnIndex - 1].toString(),
-                                            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              for (int rowIndex = 1; rowIndex <= 9; rowIndex += 1)
+                Flexible(
+                  child: Row(
+                    children: <Widget>[
+                      for (int columnIndex = 1; columnIndex <= 9; columnIndex += 1)
+                        _grid[rowIndex - 1][columnIndex - 1] != 0
+                            ? Expanded(
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: <Widget>[
+                                    Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: <Widget>[
+                                        Container(
+                                          padding: const EdgeInsets.all(8),
+                                          decoration: const BoxDecoration(shape: BoxShape.circle, color: gray),
+                                          child: Center(
+                                            child: Text(
+                                              _grid[rowIndex - 1][columnIndex - 1].toString(),
+                                              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                                            ),
                                           ),
                                         ),
-                                      ),
-                                      const SizedBox(width: 5),
-                                      (columnIndex == 9)
-                                          ? const SizedBox()
-                                          : (columnIndex % 3 != 0)
-                                              ? Container(width: 1, height: 20, color: gray)
-                                              : Container(width: 2, height: 20, color: Colors.yellow),
-                                    ],
-                                  ),
-                                  Container(width: 20, height: 1, color: gray),
-                                ],
+                                        const SizedBox(width: 5),
+                                        columnIndex == 9
+                                            ? const SizedBox()
+                                            : columnIndex % 3 != 0
+                                                ? Container(width: 1, height: 20, color: gray)
+                                                : Container(width: 2, height: 45, color: Colors.yellow),
+                                      ],
+                                    ),
+                                    Container(width: 20, height: 1, color: gray),
+                                  ],
+                                ),
+                              )
+                            : Expanded(
+                                child: Container(width: 20, height: 20),
                               ),
-                            )
-                          : Expanded(
-                              child: Container(width: 20, height: 20),
-                            ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-          ],
+            ],
+          ),
         ),
       ),
     );
