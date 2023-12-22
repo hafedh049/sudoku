ab324b151ab1f3d147fc3f0f3c2dd3a814096a9b
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-22 15:05:32 +0100

    ModifiedM lib/sudoku_grid.dart

--- lib/sudoku_grid.dart
+++ lib/sudoku_grid.dart
@@ -35,10 +35,24 @@ class _SudokuGridState extends State<SudokuGrid> {
                     for (int columnIndex = 0; columnIndex < 9; columnIndex += 1)
                       _grid[rowIndex][columnIndex] != 0
                           ? Expanded(
-                              child: Container(
-                                padding: const EdgeInsets.all(8),
-                                decoration: const BoxDecoration(shape: BoxShape.circle, color: gray),
-                                child: Center(child: Text(_grid[rowIndex][columnIndex].toString())),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: <Widget>[
+                                      Container(
+                                        padding: const EdgeInsets.all(8),
+                                        decoration: const BoxDecoration(shape: BoxShape.circle, color: gray),
+                                        child: Center(child: Text(_grid[rowIndex][columnIndex].toString())),
+                                      ),
+                                      const SizedBox(width: 5),
+                                      Container(width: 1, height: 20, color: gray),
+                                    ],
+                                  ),
+                                  const SizedBox(width: 5),
+                                  Container(width: 1, height: 20, color: gray),
+                                ],
                               ),
                             )
                           : Expanded(child: Container(width: 20, height: 20)),
