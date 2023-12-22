67e20fddbec242c4c9490a8ca5da0ea1c00e14fa
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-22 13:30:47 +0100

    Modified  lib/sudoku_grid.dart

--- lib/sudoku_grid.dart
+++ lib/sudoku_grid.dart
@@ -8,13 +8,27 @@ class SudokuGrid extends StatefulWidget {
 }
 
 class _SudokuGridState extends State<SudokuGrid> {
+  final List<String> _items = List<String>.generate(10, (int index) => index.toString())..addAll(const <String>["cell_separator","column_separator","empty_cell"]);
+  final List<String> _grid = List.generate(17*17, (int index) => "");
+
+  @override
+  void initState() {
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Row(
         children: <Widget>[
-          const Spacer(),
-          const Spacer(),
+          for (int columnIndex = 0; columnIndex < 17; columnIndex += 1)
+            Flexible(
+              child: Column(
+                children: <Widget>[for (int itemIndex = 0 ; itemIndex < 17 ; itemIndex +=1)
+                ],
+              ),
+            ),
         ],
       ),
     );
