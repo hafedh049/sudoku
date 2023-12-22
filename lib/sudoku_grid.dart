888f91d264642969fd526a106ee7045830f84aee
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-22 15:47:30 +0100

    ModifiedM lib/sudoku_grid.dart

--- lib/sudoku_grid.dart
+++ lib/sudoku_grid.dart
@@ -23,6 +23,9 @@ class _SudokuGridState extends State<SudokuGrid> {
         _grid[i].insert(j, -1);
       }
     }
+    for (int i = 1; i < _grid.length; i+= 2) {
+        _grid[i].insert(i, -1);
+     }
     print(_grid);
 
     super.initState();
@@ -37,49 +40,7 @@ class _SudokuGridState extends State<SudokuGrid> {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              /*for (int rowIndex = 1; rowIndex <= 9; rowIndex += 1)
-                Flexible(
-                  child: Row(
-                    children: <Widget>[
-                      for (int columnIndex = 1; columnIndex <= 9; columnIndex += 1)
-                        _grid[rowIndex - 1][columnIndex - 1] != 0
-                            ? Expanded(
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: <Widget>[
-                                    Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: <Widget>[
-                                        Container(
-                                          padding: const EdgeInsets.all(8),
-                                          decoration: const BoxDecoration(shape: BoxShape.circle, color: gray),
-                                          child: Center(
-                                            child: Text(
-                                              _grid[rowIndex - 1][columnIndex - 1].toString(),
-                                              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-                                            ),
-                                          ),
-                                        ),
-                                        const SizedBox(width: 5),
-                                        columnIndex == 9
-                                            ? const SizedBox()
-                                            : columnIndex % 3 != 0
-                                                ? Container(width: 1, height: 20, color: gray)
-                                                : Container(width: 2, height: 45, color: Colors.yellow),
-                                      ],
-                                    ),
-                                    rowIndex == 9
-                                        ? const SizedBox()
-                                        : rowIndex % 3 != 0
-                                            ? Container(width: 20, height: 1, color: gray)
-                                            : Container(width: 20, height: 1, color: Colors.yellow),
-                                  ],
-                                ),
-                              )
-                            : Expanded(child: Container(width: 20, height: 20)),
-                    ],
-                  ),
-                ),*/
+             for(int rowIndex = 0;)
             ],
           ),
         ),
