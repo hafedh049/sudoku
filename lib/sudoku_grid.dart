584a75053c08b009e4a74fe90f3295a2e394d30e
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-22 16:05:57 +0100

    ModifiedM lib/sudoku_grid.dart

--- lib/sudoku_grid.dart
+++ lib/sudoku_grid.dart
@@ -46,8 +46,10 @@ class _SudokuGridState extends State<SudokuGrid> {
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
               for (int columnIndex = 0; columnIndex < _grid.length; columnIndex += 1)
+                _grid[columnIndex].every((int element) => element == -2) ?Column(children: [Flexible(child: Container(width: 2,color: Colors.yellow),),],) : _grid[columnIndex].every((int element) => element == -2) ?:
                 Column(
                   children: <Widget>[for (int rowIndex = 0; rowIndex < _grid[rowIndex].length; rowIndex += 1)
+                  _grid[rowIndex][columnIndex] == -2 ?Contain :
              , ],
                 ),
             ],
